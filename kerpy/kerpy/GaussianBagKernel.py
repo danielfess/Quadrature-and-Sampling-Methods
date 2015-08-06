67220b106b83770fefed3ee16b80dacd8ab00da3
sejdino <dino.sejdinovic@gmail.com>
2015-08-06 21:56:39 +0100

    license update

--- kerpy/kerpy/GaussianBagKernel.py
+++ kerpy/kerpy/GaussianBagKernel.py
@@ -1,12 +1,3 @@
-"""
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-
-Written (W) 2015 Dino Sejdinovic
-"""
-
 from kerpy.BagKernel import BagKernel
 from abc import abstractmethod
 from numpy import exp
